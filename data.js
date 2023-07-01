db728b334b878ff5716efffc335bce7465676479
Petryakov_Sergey <petrykov2501@gmail.com>
2023-07-01 17:25:09 +0500

    JS extended ht7

---
+++ data.js
@@ -0,0 +1,70 @@
+const productData = `
+[
+    {
+        "img": "./img/pic1.png",
+        "item": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "subject":  "T-SHIRT",
+        "currency": "$",
+        "color": "Red",
+        "size": "XL",
+        "quantity": "1"
+    },
+    {
+        "img": "img/pic2.png",
+        "item": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "subject":  "T-SHIRT",
+        "currency": "$",
+        "color": "Red",
+        "size": "XL",
+        "quantity": "1"
+    },
+    {
+        "img": "img/pic3.png",
+        "item": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "subject":  "T-SHIRT",
+        "currency": "$",
+        "color": "Red",
+        "size": "XL",
+        "quantity": "1"
+    },
+    {
+        "img": "img/pic4.png",
+        "item": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "subject":  "T-SHIRT",
+        "currency": "$",
+        "color": "Red",
+        "size": "XL",
+        "quantity": "1"
+    },
+    {
+        "img": "img/pic5.png",
+        "item": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "subject":  "T-SHIRT",
+        "currency": "$",
+        "color": "Red",
+        "size": "XL",
+        "quantity": "1"
+    },
+    {
+        "img": "img/pic6.png",
+        "item": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "subject":  "T-SHIRT",
+        "currency": "$",
+        "color": "Red",
+        "size": "XL",
+        "quantity": "1"
+
+    }
+]`
